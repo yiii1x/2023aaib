08f9071a0fa733c8b5283ca92445eaddeee44be4
yiii1x <yichengzhou111@gmail.com>
2023-12-01 11:55:22 +0800

    week12

---
+++ week12/week12-7.py
@@ -0,0 +1,6 @@
+n = int(input())
+print(n, end=' ')
+if n%400==0: print('is a leap year.')
+elif n%100==0: print('is not a leap year.')
+elif n%4==0: print('is a leap year.')
+else: print('is not a leap year.')
